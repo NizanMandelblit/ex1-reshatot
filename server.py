ac2feebfbda38e4b898e729d6c03d36e45ae1b0f
shaiyis <56585128+shaiyis@users.noreply.github.com>
2020-11-09 00:33:52 +0200

    immediately return ip if static

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 import sys
 import time
 
-time_start = time.time()
+#time_start = time.time()
 seconds = 0
 myPort = sys.argv[1]
 parentIP = sys.argv[2]
@@ -10,34 +10,43 @@ parentPort = sys.argv[3]
 ipsFileName = sys.argv[4]  # the cache file
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('', 12345))
+s.bind(('', int(myPort)))
 
 while True:
 	f = open(ipsFileName, "a+")
 	f.seek(0)
 	ipRequest, addrClient = s.recvfrom(1024)
 	retLine=None
+
 	#checking the cache of the server
 	for x in f:
 		lineSplit=x.split(",")
 		if ipRequest.decode()==lineSplit[0]:
-			ttl=lineSplit[2]
-			seconds = int(time.time() - time_start)
-			print("seconds:"+str(seconds))
-			if int(ttl)>=(int(seconds)):
+			if len(lineSplit)== 3: # static
 				retLine=x
-			else:
-				print("too much time is spent!")
-				retLine=None
+				break
+			else: #dynamic
+				ttl=lineSplit[2]
+				savedAt = float(lineSplit[3])
+				seconds = int(time.time() - savedAt)
+				print("seconds:"+str(seconds))
+				if int(ttl)>= seconds:
+					retLine=x
+				else:
+					print("too much time is spent!")
+					retLine=None
+
 	#asking the parent server if its not found in the cache file
 	if retLine==None:
 		s.sendto(ipRequest,(parentIP,int(parentPort)))
-		retLine,addrParentServer=s.recvfrom(1024)
-		splitedLine=retLine.decode().split(",")
-		f.write('\n'+retLine.decode())
+		retLine,addrParentServer=s.recvfrom(1024) # we learn retLine from the parent server
+		retLine = retLine.decode()
+		retLine += "," + str(time.time())
+		#splitedLine=retLine.split(",")
+		f.write('\n'+retLine)
 		f.close()
 	s.sendto(retLine.encode(), addrClient)
 
 
 
-s.close()
+# s.close() ?
